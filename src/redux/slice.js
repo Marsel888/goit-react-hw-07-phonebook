cfee60288e21463f28d192fe0ffce8de410f2b5b
Vladislav <BJIAD7878@gmail.com>
2022-03-25 09:08:21 +0200

    new2

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -3,19 +3,9 @@ import { createSlice } from '@reduxjs/toolkit'
 export const Myslise = createSlice({
   name: 'user',
   initialState: {
-    login: [],
     filter: '',
   },
   reducers: {
-    logIn(state, action) {
-      return { ...state, login: [...state.login, action.payload] }
-    },
-    Delete(state, action) {
-      return {
-        ...state,
-        login: state.login.filter((stat) => stat.id !== action.payload),
-      }
-    },
     Fil(state, action) {
       return { ...state, filter: action.payload }
     },
